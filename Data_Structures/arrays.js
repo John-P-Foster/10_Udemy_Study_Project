3351b578ca415d79bdee90444f11140b39038ce3
John-P-Foster <johnpete.foster@gmail.com>
2025-05-23 13:00:25 -0400

    Started array study section.

---
+++ Data_Structures/arrays.js
@@ -0,0 +1,6 @@
+/* 
+ Arrays are stored in contiguous memory 
+    - making them the most compact data structure 
+    - look ups are done in constant time. 
+    - searchs are in linear time O(n)
+*/
