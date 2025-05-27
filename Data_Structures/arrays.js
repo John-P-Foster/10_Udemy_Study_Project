ad8f30011f60ebdbfd2879aeae3d002ba52ad237
John-P-Foster <johnpete.foster@gmail.com>
2025-05-27 14:17:26 -0400

    Building an array from scratch

--- Data_Structures/arrays.js
+++ Data_Structures/arrays.js
@@ -65,4 +65,56 @@
     const wizard1 = new Wizard('Shelly', 'Healer');
     const wizard2 = new Wizard('Shawn', 'Dark Magic');
 
-    wizard1.introduce();
+    // wizard1.introduce();
+
+    /*
+     How to build an Array in JavaScript
+     */
+
+     class MyArray {
+        constructor(){
+            this.length = 0; 
+            this.data = {}; 
+        }
+
+        get(index){
+            return this.data[index]
+        }
+
+        push(item){
+            this.data[this.length] = item
+            this.length++; 
+            return this.length; 
+        }
+
+        pop(){
+            const lastItem = this.data[this.length - 1];
+            delete this.data[this.length-1]; 
+            this.length--; 
+            return lastItem;
+        }
+
+        delete(index){
+            const item = this.data[index]; 
+            this.shiftItems(index); 
+        }
+
+        shiftItems(index) {
+            for(let i = index; i < this.length - 1; i++){
+                this.data[i] = this.data[i + 1]
+            }
+            delete this.data[this.length-1];
+            this.length--;
+        }
+     }
+
+     const newArray = new MyArray(); 
+     newArray.push('hi');
+     newArray.push('you');
+     newArray.push('!');
+     newArray.delete(0);
+     newArray.push('are');
+     newArray.push('nice');
+     newArray.delete(1);
+
+     console.log(newArray);
