5e4f6906886653e1d2f60e19889baefe10fe97e1
John-P-Foster <johnpete.foster@gmail.com>
2025-06-03 16:01:27 -0400

    started B trees

--- Data_Structures/trees.js
+++ Data_Structures/trees.js
@@ -9,10 +9,22 @@
 /**
  * Binary tree
  *  - A tree were each parent has only two children 
+ * 
+ *  - Perfect Binary Tree
+ *      - All nodes have either zero or two children 
+ *      - The bottom row has all nodes full
+ *      - Each level has double the nodes of the level above it. 
+ *      - the number of nodes on each level is equal to the sum of all the nodes above it plus 1.
+ *      - Can calculate the number of nodes for a given level by raising it to 2. 
+ * 
+ *  - Full Binary Tree
+ *      - All node have either zero or two children. 
  */
 
-function BinaryTreeNode(value){
-    this.value = value; 
-    this.left = null; 
-    this.right = null;
+class BinaryTreeNode{
+    constructor(value){
+        this.value = value; 
+        this.left = null; 
+        this.right = null;
+    }
 }
