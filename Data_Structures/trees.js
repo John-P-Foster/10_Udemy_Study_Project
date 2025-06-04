1429b9d1496d3a2204e9f7ab17a9136d5d8a9a60
John-P-Foster <johnpete.foster@gmail.com>
2025-06-04 14:17:00 -0400

    Creating a Binary Search Tree

--- Data_Structures/trees.js
+++ Data_Structures/trees.js
@@ -19,12 +19,93 @@
  * 
  *  - Full Binary Tree
  *      - All node have either zero or two children. 
+ * 
+ * - Binary Search Tree
+ *      - Preserves relationships
+ *      - Search, insert and delete are done in O(log N)
+ *      - Rules
+ *          - All child node to the right must be greater than the current node
+ *          - All values to the left must be less than then the parrent
  */
 
-class BinaryTreeNode{
+class Node{
     constructor(value){
         this.value = value; 
         this.left = null; 
         this.right = null;
     }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null; 
+    }
+
+    insert(value){
+        let newNode = new Node(value);
+        if(this.root ===  null){
+            this.root = newNode;
+            return; 
+        }
+        let currentNode = this.root; 
+        while(true){
+            if(newNode.value <= currentNode.value){
+                if(currentNode.left === null){
+                    currentNode.left = newNode;
+                    return; 
+                }
+                currentNode = currentNode.left; 
+            }else{
+                if(currentNode.right === null){
+                    currentNode.right = newNode; 
+                    return; 
+                }
+                currentNode = currentNode.right; 
+            }
+        }
+
+    }
+
+    lookup(value){
+        if(this.root === null){ return null};
+
+        let currentNode = this.root; 
+
+        while(currentNode){
+            if(currentNode.value === value){return currentNode;}
+            if(currentNode.value < value){
+        
+                currentNode = currentNode.right;
+            }else{
+           
+                currentNode = currentNode.left; 
+            }
+        }
+        return null; 
+    }
+}
+
+let tree = new BinarySearchTree(); 
+
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+console.log(tree.lookup(20));
+// tree.remove(170);
+console.log(JSON.stringify(traverse(tree.root)));
+//console.log(tree.lookup(20));
+//     9
+//  4     20
+// 1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
 }
