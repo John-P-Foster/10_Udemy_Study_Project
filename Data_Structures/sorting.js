c5a82a8c8040b8a38336b033a3650b830f2231fa
John-P-Foster <johnpete.foster@gmail.com>
2025-06-11 14:49:09 -0400

    Finished InsertionSort.

--- Data_Structures/sorting.js
+++ Data_Structures/sorting.js
@@ -64,3 +64,33 @@ function selectionSort(array){
 const basket = [7, 3, 5, 1, 9];
 selectionSort(basket);
 console.log(basket);
+
+/**
+ * Insertion Sort
+ *  - Best when the list is almost sorted. 
+ */
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+    const length = array.length;
+
+    for (let i = 0; i < length; i++) {
+        if (array[i] < array[0]) {
+            // Move number to the first position
+            array.unshift(array.splice(i, 1)[0]);
+        } else {
+            // Find where number should go
+            for (let j = 1; j < i; j++) {
+                if (array[i] > array[j - 1] && array[i] < array[j]) {
+                    // Move number to the right spot
+                    array.splice(j, 0, array.splice(i, 1)[0]);
+                }
+            }
+        }
+    }
+
+    return array;
+}
+
+console.log(insertionSort(numbers));
